246252b18038ebb7ade16676f96cd79a06c98407
Stephen Thompson <smt3@hotmail.com>
2019-11-16 02:04:44 -0600

    ATTOM and Zillow API combo.

---
+++ routes/attomzillow.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+var axios = require("axios");
+axios.default.defaults.headers.common["apikey"] = process.env.apikey;
+axios.default.defaults.headers.common["Accept"] = process.env.Accept;
+var Zillow = require("node-zillow");
+var zApi = new Zillow(process.env.ZWSID);
+
+var postalCode = 75218;
+var pageSize = 21;
+attomUrl =
+  "https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/address?" +
+  "postalcode=" +
+  postalCode +
+  "&pagesize=" +
+  pageSize;
+
+axios.default
+  .get(attomUrl)
+  .then(function(response) {
+    var data = response.data.property;
+    var address = [];
+    for (i = 0; i < data.length; i++) {
+      address.push(data[i].address.line1);
+    }
+    return address;
+  })
+  .catch(function(error) {
+    if (error.response) {
+      console.log("RESPONSE ERROR: ", error.message);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log("REQUEST ERROR: ", error.message);
+    }
+    console.log(error.config);
+  })
+  .then(function(address) {
+    //console.log(postalCode);
+    //console.log(address);
+    for (i = 0; i < address.length; i++) {
+      var parms = {
+        address: address[i],
+        citystatezip: postalCode
+      };
+
+      zApi
+        .get("GetDeepSearchResults", parms)
+        .then(function(results) {
+          var zpid = results.response.results.result[0].zpid;
+          return zpid;
+        })
+        .then(function(zpid) {
+          zApi
+            .get("GetUpdatedPropertyDetails", { zpid: zpid })
+            .then(function(results) {
+              console.log(zpid);
+              console.log(results.response.links);
+              console.log(results.response.images.image);
+            });
+        });
+    }
+  });
