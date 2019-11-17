3c6671e287d6c29dd7b561d952bb8dbfa2b19d88
Stephen Thompson <smt3@hotmail.com>
2019-11-17 00:37:50 -0600

    assessment details

---
+++ routes/attomzillow_assessdtl.js
@@ -0,0 +1,96 @@
+require("dotenv").config();
+var axios = require("axios");
+axios.default.defaults.headers.common["apikey"] = process.env.apikey;
+axios.default.defaults.headers.common["Accept"] = process.env.Accept;
+var Zillow = require("node-zillow");
+var zApi = new Zillow(process.env.ZWSID);
+
+var postalCode = 75218;
+var pageSize = 21;
+var minTaxAmt = 2000;
+var maxTaxAmt = 3000;
+var minMktTtlValue = 100000;
+var maxMktTtlValue = 200000;
+var maxBeds = 3;
+var maxBathsTotal = 3;
+
+attomUrl =
+  "https://api.gateway.attomdata.com/propertyapi/v1.0.0/assessment/detail?" +
+  "postalcode=" +
+  postalCode +
+  "&pagesize=" +
+  pageSize +
+  "&propertytype=SFR" +
+  "&minTaxAmt=" +
+  minTaxAmt +
+  "&maxTaxAmt=" +
+  maxTaxAmt +
+  "&minMktTtlValue=" +
+  minMktTtlValue +
+  "&maxMktTtlValue=" +
+  maxMktTtlValue +
+  "&maxBeds=" +
+  maxBeds +
+  "&maxBathsTotal=" +
+  maxBathsTotal;
+
+axios.default
+  .get(attomUrl)
+  .then(function(response) {
+    var data = response.data.property;
+    var address = [];
+    var city = [];
+    var state = [];
+    for (i = 0; i < data.length; i++) {
+      address.push(data[i].address.line1);
+    }
+    return address;
+  })
+  .catch(function(error) {
+    if (error.response) {
+      console.log("RESPONSE ERROR: ", error.message);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log("REQUEST ERROR: ", error.message);
+    }
+    console.log(error.config);
+  })
+  .then(async address => {
+    for (i = 0; i < address.length; i++) {
+      var parms = {
+        address: address[i],
+        citystatezip: postalCode
+      };
+
+      const zillowData = await getZillowData(parms, address[i]);
+    }
+  });
+
+function getZillowData(parms, address) {
+  return zApi
+    .get("GetDeepSearchResults", parms, address)
+    .then(function(results) {
+      var zpid = results.response.results.result[0].zpid;
+
+      return zpid;
+    })
+    .then(function(zpid) {
+      zApi
+        .get("GetUpdatedPropertyDetails", { zpid: zpid })
+        .then(function(results) {
+          var images = results.response.images.image[0].url;
+          console.log("\n\n----PROPERTY----");
+          console.log(address);
+          console.log(results.response.homeDescription);
+          console.log(images);
+          //loop through image array if we need it.
+          /*for (let i = 0; i < images[i].length; i++) {
+            console.log(images[i]);
+          };*/
+          console.log(results.response.links);
+          console.log("----PROPERTY----");
+          return results.response;
+        });
+    });
+}
