a3d2447e5508dab95f673b307a021854c9752b76
mikedawn <biggrayson_2000@yahoo.com>
2019-11-17 15:35:43 -0600

    Update

--- client/Component/List.js
+++ client/Component/List.js
@@ -1,59 +1,25 @@
 import React from "react";
-i
-const List = () => {
-  return (
-    <MDBContainer>
-      <MDBRow>
-        <MDBCol md="6">
-          <MDBCard>
-            <MDBCardBody>
-              <MDBCardHeader className="form-header deep-blue-gradient rounded">
-                <h3 className="my-3">
-                  <MDBIcon icon="lock" /> Login:
-                </h3>
-              </MDBCardHeader>
-              <form>
-                <div className="grey-text">
-                  <MDBInput
-                    label="Type your email"
-                    icon="envelope"
-                    group
-                    type="email"
-                    validate
-                    error="wrong"
-                    success="right"
-                  />
-                  <MDBInput
-                    label="Type your password"
-                    icon="lock"
-                    group
-                    type="password"
-                    validate
-                  />
-                </div>
+import "./List.css";
 
-              <div className="text-center mt-4">
-                <MDBBtn
-                  color="light-blue"
-                  className="mb-3"
-                  type="submit"
-                >
-                  Login
-                </MDBBtn>
-              </div>
-              </form>
-              <MDBModalFooter>
-                <div className="font-weight-light">
-                  <p>Not a member? Sign Up</p>
-                  <p>Forgot Password?</p>
-                </div>
-              </MDBModalFooter>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-  );
-};
+
+
+
+// const List = () {
+//   return (
+
+//     <UL>
+// <li>Picture {  }"module";</li>
+// <li>Price {  } "module";</li>
+// <li>Address {  } "module";</li>
+// <li>city {  } "module";</li>
+// <li>Details{  } from "module";</li>
+// </UL>
+    
+
+
+
+
+//   );
+// };
 
 export default List;
